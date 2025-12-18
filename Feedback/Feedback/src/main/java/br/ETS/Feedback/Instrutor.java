f86dc40814041c149c7e10e8059a8c88dd374dff
Emilysx <emilyg.oliveira27@gmail.com>
2025-12-18 11:20:39 -0300

    spring - 18/12/2025

--- Feedback/Feedback/src/main/java/br/ETS/Feedback/Instrutor.java
+++ Feedback/Feedback/src/main/java/br/ETS/Feedback/Instrutor.java
@@ -1,5 +1,6 @@
 package br.ETS.Feedback;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,17 +8,32 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Embeddable
+@Entity(name = "Instrutor")
+@Table (name="tbInstrutores")
+
+
 public class Instrutor {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private  int id;
+
     private String nome;
     private String email;
     private String edv;
+
+    @Enumerated(EnumType.STRING)
     private Curso curso;
+
+    @Embedded
+    private Informacoes informacoes;
 
     public Instrutor (DadosCadastroInstrutor dadosCadastroInstrutor){
         this.nome = dadosCadastroInstrutor.nome();
         this.email = dadosCadastroInstrutor.email();
         this.edv = dadosCadastroInstrutor.edv();
         this.curso = dadosCadastroInstrutor.curso();
+        this.informacoes = new Informacoes(dadosCadastroInstrutor.informacoes());
 
     }
 }
